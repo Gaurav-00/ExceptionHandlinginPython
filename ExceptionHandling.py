bddfab1d1e8322eb33a3671e5d48c7ea07b1640f
Gaurav-00 <shindegaurav5551@gmail.com>
2022-03-31 01:12:21 +0530

    Exception handling in python

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -7,6 +7,9 @@ except ZeroDivisionError as e:
     print("Exception occur"+str (e))
 '''
 try:
+    age=10
+    if age<18:
+        raise ValueError("Not eligible to vote")  #to create your own exception use this
     a=10
     b=0
     c=a/b     #ZeroDivisionError: division by zero  is the error occur in code
@@ -20,3 +23,6 @@ else:   #this code executes if no error occur in code in exception handling and 
 finally:         #Below finally code always executes
     print("Harman Ltd.")
     print("Exception Handling done")
+
+    #option to create user defined exception
+
