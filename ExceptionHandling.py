2eba2be62269caeec8ca2cf8acd105d9f7fd3307
Gaurav-00 <shindegaurav5551@gmail.com>
2022-04-05 10:29:01 +0530

    testing even or odd

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -6,6 +6,7 @@
 except ZeroDivisionError as e:
     print("Exception occur"+str (e))
 '''
+'''
 try:
     age=10
     if age<18:
@@ -19,10 +20,35 @@ except ZeroDivisionError as e:
 except :
     print("Program executed Successfully")
 else:   #this code executes if no error occur in code in exception handling and it will not work if any exception occur in code
-    print("Program execute succesfully")
-finally:         #Below finally code always executes
-    print("Harman Ltd.")
-    print("Exception Handling done")
+    print("Program execute succesfully"
 
     #option to create user defined exception
+'''
+#Automations in python
+def add(a,b):
+    return a+b
 
+   # (add(2,3))  -::if these line used then it is automation testing
+
+#unittest  By default module in python
+import unittest    #to import module
+class Myapp(unittest.TestCase):
+    #Testcase means maximum form of testing
+
+    def test_case1(self): #this is 1 test case
+        a=10
+        b=20  #if you do not want to give any instruction to function use pass
+        result=add(a,b)
+        self.assertEqual(a+b,result)
+
+    def test_case2(self):
+        a=12.5
+        b=22.5
+        result=a+b
+        self.assertEqual(a+b,result)
+
+'''finally:         #Below finally code always executes
+    print("Harman Ltd.")
+    print("Exception Handling done")'''
+if __name__=="__main__":
+    unittest.main()
