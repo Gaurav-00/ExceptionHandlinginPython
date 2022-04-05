a6e53abb7dc84e225ba4e745ade07c4192945539
Gaurav-00 <shindegaurav5551@gmail.com>
2022-04-05 12:46:00 +0530

    setup and teardown

--- test_calculate.py
+++ test_calculate.py
@@ -1,30 +1,32 @@
 import Calculate
 import unittest
 class Calculator(unittest.TestCase):
+    def setUp(self):  #it is use to initialize the values inside a class
+        self.x=30
+        self.y=24
+    def tearDown(self):   #for cleaning value
+        self.x=0          #ALWAYS use both setup and teardown
+        self.y=0
 
     def test_add(self):
-        x=10
-        y=20
-        result=Calculate.add(x,y)
-        self.assertEqual(result,x+y)
+
+        result=Calculate.add(self.x,self.y)
+        self.assertEqual(result,self.x+self.y)
 
     def test_mul(self):
-        x=10
-        y=20
-        result=Calculate.mul(x,y)
-        self.assertEqual(result,x*y)
+
+        result=Calculate.mul(self.x,self.y)
+        self.assertEqual(result,self.x*self.y)         #validate
 
     def test_sub(self):
-        x=20
-        y=10
-        result=Calculate.sub(x,y)
-        self.assertEqual(result,x-y)
+
+        result=Calculate.sub(self.x,self.y)
+        self.assertEqual(result,self.x-self.y)
 
     def test_div(self):
-        x=20
-        y=10
-        result=Calculate.div(x,y)
-        self.assertEqual(result,x/y)
+
+        result=Calculate.div(self.x,self.y)
+        self.assertEqual(result,self.x/self.y)
 
 if __name__=="__main__":
     unittest.main()
